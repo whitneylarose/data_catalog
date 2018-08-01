e0491f4d6e50c6389081d575ed5ad01e225cd177
Whitney Nelson <whitneylarose.11@gmail.com>
2018-08-01 15:36:50 -0400

    Update resource_request.py

--- scripts/resource_request.py
+++ scripts/resource_request.py
@@ -6,7 +6,7 @@ class resource_request():
    def create(self, id,key, path, r_name, r_format):
        print('Creating new resource...')
 
-       requests.post('http://128.219.187.22/api/action/resource_create',
+       requests.post('http://your-ckan-site/api/action/resource_create',
                   data={"package_id": id, "name": r_name, "format": r_format},
                   headers={"Authorization": key},
                   files=[('upload', file(path))])
