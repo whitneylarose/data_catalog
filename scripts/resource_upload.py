1484587dcb397635fea15f1ff10457e4589eb80d
Whitney Nelson <whitneylarose.11@gmail.com>
2018-08-01 15:37:50 -0400

    Update resource_upload.py

--- scripts/resource_upload.py
+++ scripts/resource_upload.py
@@ -5,7 +5,7 @@ package_id = sys.argv[1]
 file_path = sys.argv[2]
 api_key = sys.argv[3]
 
-requests.post('http://128.219.187.22/api/action/resource_create',
+requests.post('http://your-ckan-site/api/action/resource_create',
               data={"package_id":package_id},
               headers={"Authorization": api_key},
               files=[('upload', file(file_path))])
